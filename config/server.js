8b753e4e55bc0b9789b6d54a751251f89a744245
Mahmoud Saadeh <mahmoud.saadeh998@gmail.com>
2021-06-11 15:56:24 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6cd160c9318cea0cdaf564c137b62b1d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd9dbdaa5-a89b-4015-b161-2c8cecfe67fa'
+};
